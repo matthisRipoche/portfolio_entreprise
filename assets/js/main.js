a9fe2b0c9e098490d79504098d6fde6782e6d83a
matthisRipoche <matthisripoche@gmail.com>
2025-02-16 19:33:06 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// smooth scroll
+$(document).ready(function () {
+    $(".navbar .nav-link").on("click", function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                700,
+                function () {
+                    window.location.hash = hash;
+                }
+            );
+        }
+    });
+});
+
+// navbar toggle
+$("#nav-toggle").click(function () {
+    $(this).toggleClass("is-active");
+    $("ul.nav").toggleClass("show");
+});
+
+//experience scolarité
+
+$(".btns_container .btn").click(function (e) {
+    e.preventDefault();
+    if (!$(this).hasClass("active")) {
+        $(".btns_container .btn").toggleClass("active");
+        $(".experience, .scolaire").toggleClass("active");
+    }
+});
